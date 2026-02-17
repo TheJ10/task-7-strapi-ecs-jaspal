4bbf1ce3d9095b7c1cde98c4c2ff9de06f0c6d07
Jaspal Gundla <jaspalgundla@gmail.com>
2026-02-17 17:14:44 +0530

    fix: hardcode ecsInstanceRole for ecs ec2

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,13 +1,8 @@
-data "aws_iam_instance_profile" "ecs_profile" {
-  name = "ecsInstanceRole"
-}
-
-
 resource "aws_instance" "ecs" {
   ami                    = "ami-0e4d9c8f7a6b5d2c3"
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
-  iam_instance_profile = data.aws_iam_instance_profile.ecs_profile.name
+  iam_instance_profile   = "ecsInstanceRole"
   vpc_security_group_ids = [var.security_group_id]
 
   tags = {
