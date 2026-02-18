c9f774b7abdcc5458fec5df6453b0dbc3ce27d13
Jaspal Gundla <jaspalgundla@gmail.com>
2026-02-18 17:42:35 +0530

    Fix RDS DBInstanceAlreadyExists by using data source

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,22 +1,6 @@
 data "aws_db_subnet_group" "jaspal_task7_db_subnet_group" {
   name = "jaspal-task7-db-subnet-group"
 }
-
-resource "aws_db_instance" "jaspal_task7_db" {
-  identifier              = "jaspal-task7-strapi-db"
-  engine                  = "postgres"
-  engine_version          = "15"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-
-  db_name                 = var.db_name
-  username                = var.db_username
-  password                = var.db_password
-
-  publicly_accessible     = true
-  skip_final_snapshot     = true
-  deletion_protection     = false
-
-  db_subnet_group_name = data.aws_db_subnet_group.jaspal_task7_db_subnet_group.name
-
+data "aws_db_instance" "jaspal_task7_db" {
+  db_instance_identifier = "jaspal-task7-strapi-db"
 }
--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,3 +1,3 @@
 output "db_endpoint" {
-  value = aws_db_instance.jaspal_task7_db.address
+  value = data.aws_db_instance.jaspal_task7_db.address
 }
